81c11173ddde7088da18b08d08f298076f5e9ef6
helloimsc <gsiowchuen@gmail.com>
2023-03-05 17:24:08 +0800

    moved all csv files into one subfolder for easier processing later

--- reddit/reddit.py
+++ reddit/reddit.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import praw
-import argparse 
+import argparse
 import config
 from datetime import date
 
@@ -15,21 +15,46 @@ print("Query Size : ", args.size)
 
 
 ## Credential
-client_id_ = config.Credential['client_id']
-client_secret_ = config.Credential['client_secret']
-user_agent_ = config.Credential['user_agent']
-reddit = praw.Reddit(client_id=client_id_, client_secret=client_secret_, user_agent=user_agent_)
+client_id_ = config.Credential["client_id"]
+client_secret_ = config.Credential["client_secret"]
+user_agent_ = config.Credential["user_agent"]
+reddit = praw.Reddit(
+    client_id=client_id_, client_secret=client_secret_, user_agent=user_agent_
+)
 
 posts = []
 ml_subreddit = reddit.subreddit(args.query)
 for post in ml_subreddit.hot(limit=None):
-    posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
-posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
+    posts.append(
+        [
+            post.title,
+            post.score,
+            post.id,
+            post.subreddit,
+            post.url,
+            post.num_comments,
+            post.selftext,
+            post.created,
+        ]
+    )
+posts = pd.DataFrame(
+    posts,
+    columns=[
+        "title",
+        "score",
+        "id",
+        "subreddit",
+        "url",
+        "num_comments",
+        "body",
+        "created",
+    ],
+)
 
 
 today = date.today()
 str_date = today.strftime("%y%m%d")
-filename = str_date + "_" + args.query + "_" + str(args.size) +".csv"
+filename = f"csvs/{str_date}_{args.query}_{args.size}.csv"
 print("Generating CSV with filename: " + filename)
 posts.to_csv(filename)
 print("Done")
