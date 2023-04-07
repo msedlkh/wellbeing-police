d31d5b356a773d7016922c887ba8ea0bdde370ad
msedlkh <75288989+msedlkh@users.noreply.github.com>
2023-04-07 14:16:07 +0800

    Revert "David"

--- text_processing.py
+++ text_processing.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
 import re
-# import os
-# import argparse 
+import os
+import argparse 
 from datetime import date
-import emoji
 
 class text_processing:
     
@@ -12,8 +11,30 @@ class text_processing:
         pass
 
     def de_emojify(self, text):
+        # print(text)
+        regrex_pattern = re.compile(pattern = "["
+            u"\U0001F600-\U0001F64F"  # emoticons
+            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            u"\U00002500-\U00002BEF"  # chinese char
+            u"\U00002702-\U000027B0"
+            u"\U00002702-\U000027B0"
+            u"\U000024C2-\U0001F251"
+            u"\U0001f926-\U0001f937"
+            u"\U00010000-\U0010ffff"
+            u"\u2640-\u2642" 
+            u"\u2600-\u2B55"
+            u"\u200d"
+            u"\u23cf"
+            u"\u23e9"
+            u"\u231a"
+            u"\ufe0f"  # dingbats
+            u"\u3030"
+            u"\u200b"
+                            "]+", flags = re.UNICODE)
         try:
-            edited = emoji.demojize(text)
+            edited = regrex_pattern.sub(r' ',text)
         except:
             print("emoji: ",text)
             edited = text
